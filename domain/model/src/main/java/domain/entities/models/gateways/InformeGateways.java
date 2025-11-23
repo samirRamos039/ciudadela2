6094e6acf3e04c093bf3932288f7611c0ed6264f
samirRamos039 <rzagza039@gmail.com>
2025-11-23 16:03:40 -0500

    error fix

--- domain/model/src/main/java/domain/entities/models/gateways/InformeGateways.java
+++ domain/model/src/main/java/domain/entities/models/gateways/InformeGateways.java
@@ -1,12 +1,18 @@
 package domain.entities.models.gateways;
+
 import domain.entities.models.InformeConstruccion;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface InformeGateways {
     Mono<InformeConstruccion> saveInforme(InformeConstruccion informe);
+
     Flux<InformeConstruccion> getInforme();
+
     Mono<InformeConstruccion> getInformeById(int id);
+
     Mono<Void> deletInforme(int id);
+
     Mono<InformeConstruccion> updateInforme(int id);
 
 }
